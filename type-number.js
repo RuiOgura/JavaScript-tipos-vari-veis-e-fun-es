f3970a48f70f708392c594eb16c0845a622312f8
RuiOgura <jun_rui87@hotmail.com>
2023-03-09 15:25:11 -0300

    Aula 1

---
+++ type-number.js
@@ -0,0 +1,26 @@
+// Tipos NUmber
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica)
+
+
+//Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante = .5; //é igual a 0.5
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+
+//NaN - Not a Number ( Não é um Numero, não reconhece)
+
+const alura = "alura";
+console.log(alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,32 @@
+const texto1 = "Olá mundo!";
+const string = "123456";
+const citacao = ' Meu nome é ';  
+const meuNome = "Rui";
+
+
+//contatenação (+)
+
+console.log(citacao + meuNome)
+
+
+//template string OU tempate literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const senha = 'minhaSenha';
+console.log(senha.length)  //10 caracteres
+
+let isSunny = false;
+if (!isSunny) {
+  console.log("Não se esqueça de usar protetor solar!");
+}
+
+
