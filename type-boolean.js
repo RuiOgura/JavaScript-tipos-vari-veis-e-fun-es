ab177d24fd1998b7e36204ad2325f6281f4e47a2
RuiOgura <jun_rui87@hotmail.com>
2023-03-10 08:24:47 -0300

    atualizaçao

--- type-boolean.js
+++ type-boolean.js
@@ -8,3 +8,44 @@ const texto1= "Alura";
 const texto2= "alura";
 
 console.log (texto1 === texto2); //false 
+
+/*   processo no GITHUB- no terminal
+
+PS C:\Users\kodom\Desktop\JS-aula> git init
+Initialized empty Git repository in C:/Users/kodom/Desktop/JS-aula/.git/
+PS C:\Users\kodom\Desktop\JS-aula> git status
+On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        type-boolean.js
+        type-number.js
+        type-string.js
+
+nothing added to commit but untracked files present (use "git add" to track)
+PS C:\Users\kodom\Desktop\JS-aula> git remote add origin https://github.com/RuiOgura/JavaScript-tipos-vari-veis-e-fun-es.git    
+
+PS C:\Users\kodom\Desktop\JS-aula> git add .
+
+PS C:\Users\kodom\Desktop\JS-aula> git status
+On branch master
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   type-boolean.js
+        new file:   type-number.js
+        new file:   type-string.js
+
+PS C:\Users\kodom\Desktop\JS-aula> git commit -m "Aula 1"
+[master (root-commit) f3970a4] Aula 1
+ 3 files changed, 68 insertions(+)
+ create mode 100644 type-boolean.js
+ create mode 100644 type-number.js
+ create mode 100644 type-string.js
+PS C:\Users\kodom\Desktop\JS-aula> git push origin master
+
+*/
